9d0c72c1cc301fc8752304ac9f1f31a3a4ad372d
amanda-lima <amanda.limaa0202@gmail.com>
2021-04-19 21:40:22 -0300

    Preparando Banco de Dados

--- server.js
+++ server.js
@@ -11,3 +11,19 @@ app.get('/',(req,res) => {
     res.send('test')
 })
 
+let db = new sqlite3.Database('./db/articles.db', (err) => {
+    if (err) {
+        return
+        console.error(err.message);
+    }
+    db.run (`
+        CREATE TABLE IF NOT EXISTS articles(
+            id  INTEGER not null constraint articles_pk primary key autoincrement,
+            title TEXT,
+            content TEXT
+        );
+        create unique index articles_id_uindex
+            on articles (id);
+    `)
+    console.log('Conected to the SQlite database.');
+});
